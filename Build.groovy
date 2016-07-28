e0518ed1a3b848ea278533dd20300ddd2735c220
Sunjoo Park <allessunjoo.park@lge.com>
2016-07-28 10:08:17 +0900

    Check result

--- Build.groovy
+++ Build.groovy
@@ -6,44 +6,47 @@ node('master_pipeline') {
    def branch_name  = job_name_arr[1]
    def machine_name = job_name_arr[3]
 
-   stage 'Call a clean engineering build'
-   //clean-engineering-starfish-m16-build
-   def clean_job_name = "clean-engineering-starfish-" + machine_name + "-build"
-
-   join = parallel([clean: {
-        node('verification'){
-            build job:clean_job_name, parameters: [
-                [$class: 'StringParameterValue',  name:'build_codename',        value:branch_name],
-                [$class: 'StringParameterValue',  name:'token',                 value:'trigger_clean_build'],
-                [$class: 'StringParameterValue',  name:'extra_images',          value:'starfish-bdk'],
-                [$class: 'StringParameterValue',  name:'build_starfish_commit', value:'@drd4tv'],
-                [$class: 'TextParameterValue',    name:'webos_local',           value:'WEBOS_DISTRO_BUILD_ID="318"\nSDKMACHINE="i686"'],
-                [$class: 'StringParameterValue',  name:'Build_summary',         value:'test bdk build'],
-                [$class: 'BooleanParameterValue', name:'Build_starfish_m16p',   value:true],
-                [$class: 'BooleanParameterValue', name:'region_default',        value:false],
-                [$class: 'BooleanParameterValue', name:'region_atsc',           value:false],
-                [$class: 'BooleanParameterValue', name:'region_arib',           value:false],
-                [$class: 'BooleanParameterValue', name:'region_dvb',            value:false],
-            ]
-        }
-    }
-    ])
-
-
-    def clean_build_result = join.clean.result
-    def clean_build_number = join.clean.number.toString()
+   stage 'Check foss change'
+   if ( "${CLEAN_BUILD_RESULT}" == "SUCCESS" ) {
+       stage 'Call a clean engineering build'
+       //clean-engineering-starfish-m16-build
+       echo 'Clean Build'
+       def clean_job_name = "clean-engineering-starfish-" + machine_name + "-build"
     /*
-    def clean_build_result = "${CLEAN_BUILD_RESULT}"
-    def clean_build_number = "${CLEAN_BUILD_NUMBER}"
-    */
-    stage 'Copy bdk result'
-    node('verification'){
-        def bdk_job_name = "starfish-bdk"
-        def org_dir = '/binary/build_results/starfish_verifications/' + clean_job_name + '/' + clean_build_number
-        def target_root = '/binary/build_results/temp/' + bdk_job_name
-        def target_dir = target_root + '/' + "${env.BUILD_NUMBER}"+ "_bdk"
+       join = parallel([clean: {
+            node('verification'){
+                build job:clean_job_name, parameters: [
+                    [$class: 'StringParameterValue',  name:'build_codename',        value:branch_name],
+                    [$class: 'StringParameterValue',  name:'token',                 value:'trigger_clean_build'],
+                    [$class: 'StringParameterValue',  name:'extra_images',          value:'starfish-bdk'],
+                    [$class: 'StringParameterValue',  name:'build_starfish_commit', value:'@drd4tv'],
+                    [$class: 'TextParameterValue',    name:'webos_local',           value:'WEBOS_DISTRO_BUILD_ID="318"\nSDKMACHINE="i686"'],
+                    [$class: 'StringParameterValue',  name:'Build_summary',         value:'test bdk build'],
+                    [$class: 'BooleanParameterValue', name:'Build_starfish_m16p',   value:true],
+                    [$class: 'BooleanParameterValue', name:'region_default',        value:false],
+                    [$class: 'BooleanParameterValue', name:'region_atsc',           value:false],
+                    [$class: 'BooleanParameterValue', name:'region_arib',           value:false],
+                    [$class: 'BooleanParameterValue', name:'region_dvb',            value:false],
+                ]
+            }
+        }
+        ])
 
-        sh 'mkdir -p ' + target_root
-        sh 'cp -r ' + org_dir + '/ ' + target_dir
+
+        def clean_build_result = join.clean.result
+        def clean_build_number = join.clean.number.toString()
+        */
+        def clean_build_result = "${CLEAN_BUILD_RESULT}"
+        def clean_build_number = "${CLEAN_BUILD_NUMBER}"
+        stage 'Copy bdk result'
+        node('verification'){
+            def bdk_job_name = "starfish-bdk"
+            def org_dir = '/binary/build_results/starfish_verifications/' + clean_job_name + '/' + clean_build_number
+            def target_root = '/binary/build_results/temp/' + bdk_job_name
+            def target_dir = target_root + '/' + "${env.BUILD_NUMBER}"+ "_bdk"
+
+            sh 'mkdir -p ' + target_root
+            sh 'cp -r ' + org_dir + '/ ' + target_dir
+        }
     }
 }
